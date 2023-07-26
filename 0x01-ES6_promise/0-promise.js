b6f21bd6e9203560bea7e7f6c7ecc2b32e1d51f3
Hollar14 <i_lawal@outlook.com>
2023-07-26 18:23:31 +0000

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,14 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve("Resolved");
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // API call logic here
+    // ...
+    if (responseFromAPI) {
+      resolve(responseFromAPI); // resolve with API response
+    } else {
+      reject("Error: API call failed"); // reject with error message
+    }
   });
 }
+
+export default getResponseFromAPI;
+
