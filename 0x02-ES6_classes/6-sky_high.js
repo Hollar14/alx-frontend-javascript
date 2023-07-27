a5997513605a6737e85c237f576406e325f39993
Hollar14 <i_lawal@outlook.com>
2023-07-27 16:31:14 +0000

    skyhigh

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,23 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
+  /* eslint-disable */
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
+  // getter 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
-    }
-    this._floors = newFloors;
+  // setter 
+  set floors(Floors) {
+    this._floors = Floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
